68fb40459095beede85a1011215e557f6c981574
IsuruRajapaksha <hibrajapaksha@students.nsbm.ac.lk>
2023-07-30 00:00:45 +0530

    three

--- venvfastparcel/Fastparcel/Fastparcel/urls.py
+++ venvfastparcel/Fastparcel/Fastparcel/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views  # new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('customer/', views.customer_page),
+    path('courier/', views.courier_page)
 ]
