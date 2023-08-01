afec82609536fc9c50712c7799d03e14ee6be3f5
IsuruRajapaksha <hibrajapaksha@students.nsbm.ac.lk>
2023-08-02 00:56:02 +0530

    oo

--- venvfastparcel/Fastparcel/core/customer/views.py
+++ venvfastparcel/Fastparcel/core/customer/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-
+from django.urls import reverse
+from core.customer import forms
 
 @login_required()
 def home(request):
-    return render(request,'customer/base.html')
+    return redirect(reverse('customer:profile'))
+
+@login_required(login_url='/sign_in/?next=/customer/')
+def profile_page(request):
+    user_forms = forms.BasicUserForm(instance=request.user)
+
+    if request.method == 'POST':
+        user_forms = forms.BasicUserForm(request.POST, instance=request.user)
+        if user_forms.is_valid():
+            user_forms.save()
+            return redirect(reverse('customer:profile'))
+
+    return render(request, 'customer/profile.html',
+                   {'user_forms': user_forms})
+    
 
 
 #please add comments to the all function because this is a complex app
